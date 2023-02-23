6ec2c5e6110638fe8a504e43021f467a06a6ef9f
Breno Ludgero <116920906+BrenoLudgero@users.noreply.github.com>
2023-02-23 19:15:36 -0300

    Player movement and animations
    Player's movement was added with it's respective frames of animation.
    Player no longer leaves the canvas.

--- script.js
+++ script.js
@@ -5,27 +5,80 @@ const canvasWidth = canvas.width = 876;
 const canvasHeight = canvas.height = 720;
 
 gameFrame = 0;
-stagFrames = 7;
+stagFrames = 5;
 
 const sprites = new Image();
 sprites.src = "img/sprites.png";
 
-const actors = {
-    player: {
+const controls = {
+    w: {pressed: false, function() {player.animateUp(), player.moveUp()}},
+    a: {pressed: false, function() {player.animateLeft(), player.moveLeft()}},
+    s: {pressed: false, function() {player.animateDown(), player.moveDown()}},
+    d: {pressed: false, function() {player.animateRight(), player.moveRight()}}
+};
+
+document.addEventListener("keydown", (e) => {
+    if (controls[e.key]) {controls[e.key].pressed = true}
+});
+
+document.addEventListener("keyup", (e) => {
+    if (controls[e.key]) {controls[e.key].pressed = false}
+});
+
+const move = () => {
+    Object.keys(controls).forEach(key=> {
+        controls[key].pressed && controls[key].function()
+    })
+};
+
+function spriteCycle() {
+    if(gameFrame % stagFrames == 0) {
+        if (player.x < 88) {
+            player.x+=26}
+        else player.x = 10
+    }
+} 
+
+const player = {
     x: 10,
     y: 371,
     w: 14,
-    h: 22
+    h: 22,
+    px:424,
+    py:338,
+    speed: 3,
+    moveUp: function() {this.py-=this.speed},
+    moveLeft: function() {this.px-=this.speed},
+    moveDown: function() {this.py+=this.speed},
+    moveRight: function() {this.px+=this.speed},
+    animateUp: function() {if (controls.d.pressed === false && controls.a.pressed === false){this.y = 409; spriteCycle()}},
+    animateDown: function() {if (controls.d.pressed === false && controls.a.pressed === false){this.y = 371; spriteCycle()}},
+    animateLeft: function() {this.y = 445; spriteCycle()},
+    animateRight: function() {this.y = 478; spriteCycle()},
+};
+
+function playableArea () {
+    if (player.py <= 1) {
+        player.py = 1
     }
-}
+    if (player.py >= 692) {
+        player.py = 692
+    }
+    if (player.px <= 0) {
+        player.px = 0
+    }
+    if (player.px >= 858) {
+        player.px = 858
+    }
+};
 
 function animate() {
-    context.drawImage(sprites, actors.player.x, actors.player.y, actors.player.w, actors.player.h, 424, 338, actors.player.w * 1.3, actors.player.h * 1.3);
-    if (gameFrame % stagFrames == 0) {
-        if (actors.player.x < 88) actors.player.x+=26;
-        else actors.player.x = 10;
-    }
+    move();
+    playableArea();
+    context.clearRect(0, 0, canvasWidth, canvasHeight);
+    context.drawImage(sprites, player.x, player.y, player.w, player.h, player.px, player.py, player.w * 1.3, player.h * 1.3);
     gameFrame++;
     requestAnimationFrame(animate)
 };
+
 animate();
