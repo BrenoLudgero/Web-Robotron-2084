f908dee1f173a60bf5ca3b9dfa91777464a30fbb
BrenoLudgero <breno.ludgero@yahoo.com>
2024-01-18 09:01:17 -0300

    Split Input from Input Manager

--- src/modules/managers/input-mngr.js
+++ src/modules/managers/input-mngr.js
@@ -1,49 +1,19 @@
 export {InputManager};
+import {Input} from "../models/input.js";
 
 class InputManager {
-    constructor(game) { // REMOVE ALL GAME WITH DEBUGGER
-        this.keysPressed = [];
-        this.playerControls = ["w", "a", "s", "d", "arrowup", "arrowleft", "arrowdown", "arrowright"];
-        this.listenToPlayerControls(game);
+    constructor(game) { // REMOVE GAME WITH DEBUGGER
+        this.input = new Input(game);
     }
     update(player) {
         this.processMovementKeys(player);
         this.processShootingKeys(player);
     }
     pressing(key) {
-        return this.keysPressed.includes(key);
+        return this.input.keysPressed.includes(key);
     }
     pressingCombination(keys) {
         return keys.every(key => this.pressing(key));
-    }
-    unstoredPlayerKey(key) {
-        return this.playerControls.includes(key) && !this.pressing(key);
-    }
-    // Stores one instance of each pressed playerControls key in keysPressed
-    handleKeyDown(event, keysPressed) {
-        const key = event.key.toLowerCase();
-        if (this.unstoredPlayerKey(key)) {
-            keysPressed.push(key);
-        }
-    }
-    keyUnpressed(keyIndex) {
-        return keyIndex > -1;
-    }
-    // Removes the key from keysPressed when unpressed
-    handleKeyUp(event, game, keysPressed) {
-        const key = event.key.toLowerCase();
-        const keyIndex = keysPressed.indexOf(key);
-        if (this.keyUnpressed(keyIndex)) {
-            keysPressed.splice(keyIndex, 1);
-        }
-        game.debuggerr.processDebugKeys(key);
-    }
-    listenToPlayerControls(game) {
-        const {keysPressed} = this;
-        window.addEventListener("keydown", (event) => this.handleKeyDown(event, keysPressed));
-        window.addEventListener("keyup", (event) => this.handleKeyUp(event, game, keysPressed));
-        //window.addEventListener("mousedown", (event) => console.log("CLICK"));
-        //window.addEventListener("mouseup", (event) => console.log("NO CLICK"));
     }
     processKeyFunction(player, key, method) {
         if (this.pressing(key)) {
---
+++ src/modules/models/input.js
@@ -0,0 +1,38 @@
+export {Input};
+
+class Input {
+    constructor(game) { // REMOVE GAME WITH DEBUGGER
+        this.keysPressed = [];
+        this.playerControls = ["w", "a", "s", "d", "arrowup", "arrowleft", "arrowdown", "arrowright"];
+        this.listenToPlayerControls(game);
+    }
+    unstoredPlayerKey(key) {
+        return this.playerControls.includes(key) && !this.keysPressed.includes(key);
+    }
+    // Stores one instance of each pressed playerControls key in keysPressed
+    handleKeyDown(event, keysPressed) {
+        const key = event.key.toLowerCase();
+        if (this.unstoredPlayerKey(key)) {
+            keysPressed.push(key);
+        }
+    }
+    keyUnpressed(keyIndex) {
+        return keyIndex > -1;
+    }
+    // Removes the key from keysPressed when unpressed
+    handleKeyUp(event, game, keysPressed) {
+        const key = event.key.toLowerCase();
+        const keyIndex = keysPressed.indexOf(key);
+        if (this.keyUnpressed(keyIndex)) {
+            keysPressed.splice(keyIndex, 1);
+        }
+        game.debuggerr.processDebugKeys(key);
+    }
+    listenToPlayerControls(game) {
+        const {keysPressed} = this;
+        window.addEventListener("keydown", (event) => this.handleKeyDown(event, keysPressed));
+        window.addEventListener("keyup", (event) => this.handleKeyUp(event, game, keysPressed));
+        //window.addEventListener("mousedown", (event) => console.log("CLICK"));
+        //window.addEventListener("mouseup", (event) => console.log("NO CLICK"));
+    }
+}
