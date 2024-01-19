0126a1c1dba8e626bfeeb529f82409a2df4a4e2e
BrenoLudgero <breno.ludgero@yahoo.com>
2024-01-19 13:30:57 -0300

    Update Mommy hitboxes

--- src/modules/actors/humans/mommy.js
+++ src/modules/actors/humans/mommy.js
@@ -10,18 +10,18 @@ class Mommy extends Human {
                 torso: {width: 14, height: 10, xPosition: 4, yPosition: 12},
                 rightArm: {width: 3, height: 10, xPosition: 0, yPosition: 12},
                 leftArm: {width: 3, height: 10, xPosition: 18, yPosition: 12},
-                legs: {width: 10, height: 13, xPosition: 5, yPosition: 23}
+                legs: {width: 9, height: 13, xPosition: 6, yPosition: 23}
             },
             "down": {},
             "left": {
-                head: {xPosition: 5, yPosition: 5},
-                torso: {width: 9, height: 9, xPosition: 5, yPosition: 14},
+                head: {yPosition: 5},
+                torso: {width: 9, height: 9, xPosition: 6, yPosition: 14},
                 leftArm: {width: 0, height: 0, xPosition: this.centerX, yPosition: this.centerY},
                 rightArm: {width: 0, height: 0, xPosition: this.centerX, yPosition: this.centerY},
                 legs: {width: 7, height: 19, xPosition: 6, yPosition: 21}
             },
             "right": {
-                head: {xPosition: 6, yPosition: 3},
+                head: {yPosition: 3},
                 torso: {width: 9, height: 9, xPosition: 6, yPosition: 13},
                 leftArm: {width: 0, height: 0, xPosition: this.centerX, yPosition: this.centerY},
                 rightArm: {width: 0, height: 0, xPosition: this.centerX, yPosition: this.centerY},
