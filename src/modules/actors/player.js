103d787f24df0c8a9d5bd4ec075bd49f5fdbb7a1
BrenoLudgero <breno.ludgero@yahoo.com>
2024-01-21 16:48:14 -0300

    Update Player initial screen position

--- src/modules/actors/player.js
+++ src/modules/actors/player.js
@@ -4,8 +4,8 @@ import {Actor} from "../models/actor.js";
 class Player extends Actor {
     constructor(game) {
         super(game, 14, 24);
-        this.screenX = (game.ui.canvas.width / 2) - this.width;
-        this.screenY = (game.ui.canvas.height / 2) - this.height;
+        this.screenX = (game.ui.canvas.width / 2) - this.originalWidth;
+        this.screenY = (game.ui.canvas.height / 2) - this.originalHeight;
         this.lives = 3; // Updated in collisionMngr.checkPlayerCollision
         this.movementSpeed = 3.8;
         this.animationDelay = 2;
