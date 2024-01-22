239f0533dfcbfcf3d346d7a880bcea17e9f1ce92
BrenoLudgero <breno.ludgero@yahoo.com>
2024-01-22 16:29:45 -0300

    Move hitbox methods to Hitbox Manager

--- src/modules/managers/collision-mngr.js
+++ src/modules/managers/collision-mngr.js
@@ -3,46 +3,19 @@ import {isActorOfType} from "../helpers/globals.js";
 
 class CollisionManager {
     update(game) {
-        const {actorMngr, projectileMngr, debuggerr} = game;
+        const {actorMngr, projectileMngr, hitboxMngr, debuggerr} = game;
         const {player, enemies, humans} = actorMngr.actors;
         if (!debuggerr.actorInvincibility) {
-            this.checkAllCollisions(player, enemies, humans, projectileMngr);
+            this.checkAllCollisions(player, enemies, humans, projectileMngr, hitboxMngr);
         }
     }
-    checkAllCollisions(player, enemies, humans, projectileMngr) {
-        this.checkPlayerCollisions(player, enemies);
-        this.checkHumanCollisions(player, enemies, humans);
-        this.checkProjectileCollisions(projectileMngr.projectiles, enemies);
+    checkAllCollisions(player, enemies, humans, projectileMngr, hitboxMngr) {
+        this.checkPlayerCollisions(player, enemies, hitboxMngr);
+        this.checkHumanCollisions(player, enemies, humans, hitboxMngr);
+        this.checkProjectileCollisions(projectileMngr.projectiles, enemies, hitboxMngr);
     }
     isProjectile(actor) {
         return actor.angle !== undefined;
-    }
-    getLimbHitbox(actor, limb) {
-        const hitbox = actor.hitboxes[limb];
-        return {
-            left: actor.screenX + hitbox.xPosition,
-            right: (actor.screenX + hitbox.xPosition) + hitbox.width,
-            top: actor.screenY + hitbox.yPosition,
-            bottom: (actor.screenY + hitbox.yPosition) + hitbox.height
-        };
-    }
-    getRotatedHitbox(projectile) {
-        const halfWidth = projectile.width / 2;
-        const halfHeight = projectile.height / 2;
-        const centerX = projectile.screenX + halfWidth;
-        const centerY = projectile.screenY + halfHeight;
-        const cosAngle = Math.cos(projectile.angle);
-        const sinAngle = Math.sin(projectile.angle);
-        const rotatedX1 = (centerX - (halfWidth * Math.abs(cosAngle))) - (halfHeight * Math.abs(sinAngle));
-        const rotatedX2 = (centerX + (halfWidth * Math.abs(cosAngle))) + (halfHeight * Math.abs(sinAngle));
-        const rotatedY1 = (centerY - (halfWidth * Math.abs(sinAngle))) - (halfHeight * Math.abs(cosAngle));
-        const rotatedY2 = (centerY + (halfWidth * Math.abs(sinAngle))) + (halfHeight * Math.abs(cosAngle));
-        return {
-            left: Math.min(rotatedX1, rotatedX2),
-            right: Math.max(rotatedX1, rotatedX2),
-            top: Math.min(rotatedY1, rotatedY2),
-            bottom: Math.max(rotatedY1, rotatedY2)
-        };
     }
     collisionDetected(actor, target) {
         return (
@@ -53,20 +26,20 @@ class CollisionManager {
         );
     }
     // Checks collision between two actors
-    checkSingleCollision(actor, target) {
+    checkSingleCollision(actor, target, hitboxMngr) {
         if (this.isProjectile(actor)) {
-            const projectileHitbox = this.getRotatedHitbox(actor);
+            const projectileHitbox = hitboxMngr.getRotatedHitbox(actor);
             for (const targetLimb in target.hitboxes) {
-                const targetHitbox = this.getLimbHitbox(target, targetLimb);
+                const targetHitbox = hitboxMngr.getLimbHitbox(target, targetLimb);
                 if (this.collisionDetected(projectileHitbox, targetHitbox)) {
                     return true;
                 }
             }
         } else {
             for (const actorLimb in actor.hitboxes) {
-                const actorHitbox = this.getLimbHitbox(actor, actorLimb);
+                const actorHitbox = hitboxMngr.getLimbHitbox(actor, actorLimb);
                 for (const targetLimb in target.hitboxes) {
-                    const targetHitbox = this.getLimbHitbox(target, targetLimb);
+                    const targetHitbox = hitboxMngr.getLimbHitbox(target, targetLimb);
                     if (this.collisionDetected(actorHitbox, targetHitbox)) {
                         return true;
                     }
@@ -74,27 +47,27 @@ class CollisionManager {
             }
         }
     }
-    checkPlayerCollisions(player, enemies) {
+    checkPlayerCollisions(player, enemies, hitboxMngr) {
         for (const enemy of enemies) {
-            if (this.checkSingleCollision(player, enemy)) {
+            if (this.checkSingleCollision(player, enemy, hitboxMngr)) {
                 player.updateState("destroyed");
                 break;
             }
         }
     }
-    checkHumanPlayerCollision(humans, player) {
+    checkHumanPlayerCollision(humans, player, hitboxMngr) {
         for (const human of humans) {
-            if (this.checkSingleCollision(player, human)) {
+            if (this.checkSingleCollision(player, human, hitboxMngr)) {
                 human.updateState("rescued");
                 break;
             }
         }
     }
-    checkHumanEnemyCollision(humans, enemies) {
+    checkHumanEnemyCollision(humans, enemies, hitboxMngr) {
         for (const enemy of enemies) {
             if (isActorOfType(enemy, "Hulk")) {
                 for (const human of humans) {
-                    if (this.checkSingleCollision(human, enemy)) {
+                    if (this.checkSingleCollision(human, enemy, hitboxMngr)) {
                         human.updateState("destroyed");
                         break;
                     }
@@ -102,21 +75,28 @@ class CollisionManager {
             }
         }
     }
-    checkHumanCollisions(player, enemies, humans) {
-        this.checkHumanPlayerCollision(humans, player);
-        this.checkHumanEnemyCollision(humans, enemies);
+    checkHumanCollisions(player, enemies, humans, hitboxMngr) {
+        this.checkHumanPlayerCollision(humans, player, hitboxMngr);
+        this.checkHumanEnemyCollision(humans, enemies, hitboxMngr);
     }
     // Checks collision between all projectiles and enemies
-    checkProjectileCollisions(projectiles, enemies) {
+    checkProjectileCollisions(projectiles, enemies, hitboxMngr) {
         for (const projectile of projectiles.player) {
             for (const enemy of enemies) {
-                if (this.checkSingleCollision(projectile, enemy)) {
+                if (this.checkSingleCollision(projectile, enemy, hitboxMngr)) {
                     if (!isActorOfType(enemy, "Hulk")) {
                         enemy.updateState("destroyed");
                     } else {
                         projectile.knockback(enemy);
                     }
                     projectile.mustDelete = true;
+                    break;
+                }
+            }
+            for (const enemyProjectile of projectiles.enemies) {
+                if (this.checkSingleCollision(projectile, enemyProjectile, hitboxMngr)) {
+                    projectile.mustDelete = true;
+                    enemyProjectile.mustDelete = true;
                     break;
                 }
             }
--- src/modules/managers/hitbox-mngr.js
+++ src/modules/managers/hitbox-mngr.js
@@ -21,4 +21,31 @@ class HitboxManager {
             actor.hitboxes[limb][property] = properties[property];
         });
     }
+    getLimbHitbox(actor, limb) {
+        const hitbox = actor.hitboxes[limb];
+        return {
+            left: actor.screenX + hitbox.xPosition,
+            right: (actor.screenX + hitbox.xPosition) + hitbox.width,
+            top: actor.screenY + hitbox.yPosition,
+            bottom: (actor.screenY + hitbox.yPosition) + hitbox.height
+        };
+    }
+    getRotatedHitbox(projectile) {
+        const halfWidth = projectile.width / 2;
+        const halfHeight = projectile.height / 2;
+        const centerX = projectile.screenX + halfWidth;
+        const centerY = projectile.screenY + halfHeight;
+        const cosAngle = Math.cos(projectile.angle);
+        const sinAngle = Math.sin(projectile.angle);
+        const rotatedX1 = (centerX - (halfWidth * Math.abs(cosAngle))) - (halfHeight * Math.abs(sinAngle));
+        const rotatedX2 = (centerX + (halfWidth * Math.abs(cosAngle))) + (halfHeight * Math.abs(sinAngle));
+        const rotatedY1 = (centerY - (halfWidth * Math.abs(sinAngle))) - (halfHeight * Math.abs(cosAngle));
+        const rotatedY2 = (centerY + (halfWidth * Math.abs(sinAngle))) + (halfHeight * Math.abs(cosAngle));
+        return {
+            left: Math.min(rotatedX1, rotatedX2),
+            right: Math.max(rotatedX1, rotatedX2),
+            top: Math.min(rotatedY1, rotatedY2),
+            bottom: Math.max(rotatedY1, rotatedY2)
+        };
+    }
 }
